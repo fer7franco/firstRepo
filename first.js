ddb0d4c9dffa570b7487839a063ef046ae7ca080
Fernanda Franco <luisafernandaf7@gmail.com>
2024-01-03 14:58:03 -0500

    agregue el archivo first.js

---
+++ first.js
@@ -0,0 +1,7 @@
+//primer repositorio en Git
+// en la terminal escribirmos git init para crear un repositorio en nuestro computador 
+// copiamos la 6t linea git remote add origin https://github.com/fer7franco/firstRepo.git
+// hago cambios si es necesario en los archivos  
+// luego escribo en la terminal git status enter y me muestra los archivos que no estan siendo traqueados aparecen en rojo 
+// entonces para que nos comienza a traquear lo que hacemos es enviar al comando git add y el archivo quenos aparece en rojo O git add . para agregarlos todos 
+// paso sigueinte escribimos git commit -m que es un mensaje descriptivo entonces ahi escribimos agregue y el nombre del archivo 
