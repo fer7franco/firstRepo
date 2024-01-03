47899e32d019cf91f539e22ca8a7384cd3f45c77
Fernanda Franco <luisafernandaf7@gmail.com>
2024-01-03 15:09:59 -0500

    agregue carpeta css

--- first.js
+++ first.js
@@ -3,5 +3,8 @@
 // copiamos la 6t linea git remote add origin https://github.com/fer7franco/firstRepo.git
 // hago cambios si es necesario en los archivos  
 // luego escribo en la terminal git status enter y me muestra los archivos que no estan siendo traqueados aparecen en rojo 
-// entonces para que nos comienza a traquear lo que hacemos es enviar al comando git add y el archivo quenos aparece en rojo O git add . para agregarlos todos 
-// paso sigueinte escribimos git commit -m que es un mensaje descriptivo entonces ahi escribimos agregue y el nombre del archivo 
+// entonces para que nos comienza a traquear lo que hacemos  comin enviamos un git add y el archivo quenos aparece en rojo O git add . para agregarlos todos los qu eaparezcan
+// paso sigueinte escribimos git commit -m con un mensaje descriptivo de lo que acabamos de hacer 
+
+// lo que nos queda es subir el archivo a GITHUB porque lo que hicimos anteriormente fue solo en maquina
+// antes de esto aplicamos un git status para ver que todo este bien 
